b175e9c5bc7ca71d8da2e029e706006505048db1
Tiago Magalhaes <tiagojsmagalhaes@gmail.com>
2021-08-27 01:47:14 +0100

    started working on window class

--- src/Window.cpp
+++ src/Window.cpp
@@ -2,8 +2,35 @@
 
 namespace hvrt
 {
+
+    LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+    {
+        switch (uMsg)
+        {
+            case WM_DESTROY:
+                PostQuitMessage(0);
+                return 0;
+
+            case WM_PAINT:
+            {
+                PAINTSTRUCT ps;
+                HDC hdc = BeginPaint(hwnd, &ps);
+                FillRect(hdc, &ps.rcPaint, (HBRUSH) (COLOR_WINDOW+1));
+                EndPaint(hwnd, &ps);
+            }
+            return 0;
+
+        }
+        return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    }
+
     Window::Window(HINSTANCE instance)
     {
+        const char window_class_name[] = "Window class";
 
+        WNDCLASS window_class = {};
+        window_class.lpszClassName = window_class_name;
+        window_class.hInstance = instance;
+        window_class.lpfnWndProc = WindowProc;
     }
 }
