efca9dac4e45b806d68b4d6be77a306ec3c450fb
Tiago Magalhaes <tiagojsmagalhaes@gmail.com>
2021-09-24 23:51:45 +0100

    sample way of creating console

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,26 @@
 #include <windows.h>
 #include <iostream>
 
+#include <io.h>
+#include <fcntl.h>
 #include <Core/Core.h>
 
 int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmdshow)
 {
     using namespace hvrt;
     Window window(hInst, L"window1", 1280, 720);
+
+    AllocConsole();
+
+    FILE* instream;
+    FILE* outstream;
+    FILE* errstream;
+
+    freopen_s(&instream, "CONIN$", "r", stdin);//replace stdin with the current console input buffer
+    freopen_s(&outstream, "CONOUT$", "w", stdout);//replace stdout with the current console output buffer
+    freopen_s(&errstream, "CONOUT$", "w", stderr);//replace stderr with the current console output buffer
+
+    std::cout << "test" << std::endl;
 
     while(window.open)
     {
