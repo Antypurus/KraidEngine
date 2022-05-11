f852b61cc251b859d314d82543708f604554598f
Tiago Magalhaes <tiagojsmagalhaes@gmail.com>
2022-05-11 22:57:47 +0100

    added placeholder shader that can be used for now, should be removed when propper shaders are written

---
+++ shader.hlsl
@@ -0,0 +1,30 @@
+struct VS_OUTPUT
+{
+	float4 position:SV_POSITION;
+};
+
+struct VS_INPUT
+{
+	float3 pos:POSITION;
+};
+
+VS_OUTPUT VSMain(VS_INPUT input)
+{
+	VS_OUTPUT output;
+    
+	output.position = float4(input.pos,1.0f);
+    
+    return output;
+}
+
+struct PS_OUTPUT
+{
+    float4 color: SV_TARGET;
+};
+
+PS_OUTPUT PSMain(VS_OUTPUT input)
+{
+    PS_OUTPUT ret;
+    ret.color = float4(1.0f,1.0f,0.0f,1.0f);
+    return ret;
+};
