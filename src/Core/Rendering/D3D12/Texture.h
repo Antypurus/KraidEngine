1175599749e0cb531fac17b7ba132b2e2b9591b0
Tiago Magalhaes <tiagojsmagalhaes@gmail.com>
2022-05-19 00:24:09 +0100

    started working on texture sampling

--- src/Core/Rendering/D3D12/Texture.h
+++ src/Core/Rendering/D3D12/Texture.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <Core/Rendering/D3D12/D3D12.h>
 #include <Core/Rendering/D3D12/Resource/Resource.h>
 #include <Core/DataStructures/StringView.h>
 
@@ -8,6 +9,27 @@ namespace Kraid
 
 namespace D3D12
 {
+
+    enum class TextureAddressingMode
+    {
+        Wrap = D3D12_TEXTURE_ADDRESS_MODE_WRAP,
+        Mirror = D3D12_TEXTURE_ADDRESS_MODE_MIRROR,
+        Border = D3D12_TEXTURE_ADDRESS_MODE_BORDER,
+        Clamp = D3D12_TEXTURE_ADDRESS_MODE_CLAMP
+    };
+
+    enum class TextureSamplingMode
+    {
+        Point = D3D12_FILTER_MIN_MAG_MIP_POINT,
+        Linear = D3D12_FILTER_MIN_MAG_MIP_LINEAR,
+        Anisotropic = D3D12_FILTER_ANISOTROPIC
+    };
+
+    class TextureSampler
+    {
+    public:
+        TextureSampler() = default;
+    };
 
     class Texture
     {
