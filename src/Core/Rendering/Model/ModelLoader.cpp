6a1ca34f1207f651cd0155484cddd48646a1d80f
Tiago Magalhaes <tiagojsmagalhaes@gmail.com>
2022-06-06 00:52:00 +0100

    textures are now correctly mapped, weird z-fighting still there

--- src/Core/Rendering/Model/ModelLoader.cpp
+++ src/Core/Rendering/Model/ModelLoader.cpp
@@ -51,67 +51,67 @@ namespace Kraid
 
         std::vector<Vertex> vertices;
         std::unordered_map<UINT, UINT> index_map;
-        std::vector<Submesh> meshes;
+        std::unordered_map<uint64, std::vector<uint32>> submesh_indices;//NOTE(Tiago):material id -> indices, submeshes are defined by their material
+        std::vector<Submesh> submeshes;
         for (auto& shape : shapes)
         {
-            std::vector<uint32> indices;
-            uint32 mat_index = 0xFFFFFFFF;
-            if(shape.mesh.material_ids.size() > 0)
+            uint64 index_offset = 0;
+            for(uint64 i = 0; i < shape.mesh.num_face_vertices.size(); i++)
             {
-                mat_index = shape.mesh.material_ids[0];
-            }
-            for(uint64 i = 0; i < shape.mesh.indices.size(); ++i)
-            {
-                if(index_map.contains(shape.mesh.indices[i].vertex_index))
+                uint8 vertices_in_face =  shape.mesh.num_face_vertices[i];
+                for(uint8 j = 0; j < vertices_in_face; ++j)
                 {
-                    //index is already present in the vertex array, just take its index
-                    uint32 index = index_map[shape.mesh.indices[i].vertex_index];
-                    indices.push_back(index);
-                }
-                else
-                {
-                    //index is not present in the vertex array, create new vertex and add it
-                    uint64 position_index = shape.mesh.indices[i].vertex_index;
-                    uint64 normal_index = shape.mesh.indices[i].normal_index;
-                    uint64 uv_index = shape.mesh.indices[i].texcoord_index;
+                    uint64 vertex_index = shape.mesh.indices[index_offset + j].vertex_index;
+                    uint64 normal_index = shape.mesh.indices[index_offset + j].normal_index;
+                    uint64 uv_index = shape.mesh.indices[index_offset + j].texcoord_index;
+                    uint64 material_id = shape.mesh.material_ids[i];
+                    
+                    if(index_map.contains(vertex_index))
+                    {
+                        uint64 index = index_map[vertex_index];
+                        submesh_indices[material_id].push_back(index);
+                    }
+                    else
+                    {
+                        Vertex vert;
+                        vert.position = XMFLOAT3(
+                                attribute.vertices[vertex_index * 3],
+                                attribute.vertices[vertex_index * 3 + 1],
+                                attribute.vertices[vertex_index * 3 + 2]);
+                        vert.color = XMFLOAT3(
+                                attribute.colors[vertex_index * 3],
+                                attribute.colors[vertex_index * 3 + 1],
+                                attribute.colors[vertex_index * 3 + 1]);
+                        vert.normal = XMFLOAT3(
+                                attribute.normals[normal_index * 3],
+                                attribute.normals[normal_index * 3 + 1],
+                                attribute.normals[normal_index * 3 + 2]);
+                        vert.texture_coordinates = XMFLOAT2(
+                                attribute.texcoords[uv_index * 2],
+                                1.0f - attribute.texcoords[uv_index * 2 + 1]);
 
-                    Vertex vert;
-                    vert.position = XMFLOAT3(
-                                attribute.vertices[position_index * 3],
-                                attribute.vertices[position_index * 3 + 1],
-                                attribute.vertices[position_index * 3 + 2]);
-                    vert.color = XMFLOAT3(
-                            attribute.colors[position_index * 3],
-                            attribute.colors[position_index * 3 + 1],
-                            attribute.colors[position_index * 3 + 1]);
-                    vert.normal = XMFLOAT3(
-                            attribute.normals[normal_index * 3],
-                            attribute.normals[normal_index * 3 + 1],
-                            attribute.normals[normal_index * 3 + 2]);
-                    vert.texture_coordinates = XMFLOAT2(
-                            attribute.texcoords[uv_index * 2],
-                            1.0f - attribute.texcoords[uv_index * 2 + 1]);
+                        vertices.push_back(vert);
+                        index_map[vertex_index] = vertices.size() - 1;
+                        submesh_indices[material_id].push_back(vertices.size() - 1);
+                    }
 
-                    vertices.push_back(vert);
-                    index_map[shape.mesh.indices[i].vertex_index] = vertices.size() - 1;
-                    indices.push_back(vertices.size() - 1);
                 }
+                index_offset += vertices_in_face;
             }
-            BlinPhongMaterial mat;
-            std::string normal_map_path = "";
-            if(mat_index != UINT_MAX)
-            {
-                mat = mats[mat_index];
-                normal_map_path = materials[mat_index].normal_texname;
-            }
+        }
+
+        for(auto& submesh: submesh_indices)
+        {
+            uint64 material_id = submesh.first;
+            std::string normal_map_path = materials[material_id].normal_texname;
             if(!normal_map_path.empty())
             {
                 normal_map_path = basedir + "/" + normal_map_path;
             }
-            meshes.emplace_back(device, command_list, indices, mat, Transform(), normal_map_path);
+            submeshes.emplace_back(device, command_list, submesh.second, mats[material_id], Transform(), normal_map_path);
         }
 
-        return Model(device, command_list, meshes, vertices);
+        return Model(device, command_list, submeshes, vertices);
     }
 
 }
