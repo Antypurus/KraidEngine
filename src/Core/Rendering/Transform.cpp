9e723eb662d00f14efc47a23e4abb631ab2ee100
Tiago Magalhaes <tiagojsmagalhaes@gmail.com>
2022-07-04 00:09:05 +0100

    Begin work on improving Transform API, which will culminate in performance improvements

--- src/Core/Rendering/Transform.cpp
+++ src/Core/Rendering/Transform.cpp
@@ -4,6 +4,43 @@
 namespace Kraid
 {
 
+    Transform::Transform(const Transform& other)
+    {
+        this->translation[0] = other.translation[0];
+        this->translation[1] = other.translation[1];
+        this->translation[2] = other.translation[2];
+        
+        this->rotation[0] = other.rotation[0];
+        this->rotation[1] = other.rotation[1];
+        this->rotation[2] = other.rotation[2];
+            
+        this->scale[0] = other.scale[0];
+        this->scale[1] = other.scale[1];
+        this->scale[2] = other.scale[2];
+
+        this->has_changed = true;
+    }
+
+    Transform::Transform(
+            const float translation[3],
+            const float scale[3],
+            const float rotation[3])
+    {
+        this->translation[0] = translation[0];
+        this->translation[1] = translation[1];
+        this->translation[2] = translation[2];
+            
+        this->rotation[0] = rotation[0];
+        this->rotation[1] = rotation[1];
+        this->rotation[2] = rotation[2];
+            
+        this->scale[0] = scale[0];
+        this->scale[1] = scale[1];
+        this->scale[2] = scale[2];
+
+        this->has_changed = true;
+    }
+
     XMMATRIX Transform::GetModelMatrix() const
     {
         XMMATRIX ret = XMMatrixIdentity();
@@ -39,6 +76,43 @@ namespace Kraid
         ret.rotation[2] += other.rotation[2];
 
         return ret;
+    }
+
+    Transform& Transform::operator=(const Transform &other)
+    {
+        this->translation[0] = other.translation[0];
+        this->translation[1] = other.translation[1];
+        this->translation[2] = other.translation[2];
+        
+        this->rotation[0] = other.rotation[0];
+        this->rotation[1] = other.rotation[1];
+        this->rotation[2] = other.rotation[2];
+            
+        this->scale[0] = other.scale[0];
+        this->scale[1] = other.scale[1];
+        this->scale[2] = other.scale[2];
+
+        this->has_changed = true;
+
+        return *this;
+    }
+
+    void Transform::SetTranslation(const float x, const float y, const float z)
+    {
+        this->translation[0] = x;
+        this->translation[1] = y;
+        this->translation[2] = z;
+
+        this->has_changed = true;
+    }
+
+    void Transform::TranslateBy(const float x, const float y, const float z)
+    {
+        this->translation[0] += x;
+        this->translation[1] += y;
+        this->translation[2] += z;
+
+        this->has_changed = true;
     }
 
 }
--- src/Core/Rendering/Transform.h
+++ src/Core/Rendering/Transform.h
@@ -13,59 +13,27 @@ namespace Kraid
         float translation[3] = {0,0,0};
         float scale[3] = {1,1,1};
         float rotation[3] = {0,0,0};
-
+    private:
+        mutable bool has_changed = true;
+        mutable XMMATRIX cached_model_matrix = XMMatrixIdentity();
+    public:
         Transform() = default;
-        Transform(const Transform& other)
-        {
-            this->translation[0] = other.translation[0];
-            this->translation[1] = other.translation[1];
-            this->translation[2] = other.translation[2];
-            
-            this->rotation[0] = other.rotation[0];
-            this->rotation[1] = other.rotation[1];
-            this->rotation[2] = other.rotation[2];
-            
-            this->scale[0] = other.scale[0];
-            this->scale[1] = other.scale[1];
-            this->scale[2] = other.scale[2];
-        }
-
+        Transform(const Transform& other);
         Transform(
                 const float translation[3],
                 const float scale[3],
-                const float rotation[3])
-        {
-            this->translation[0] = translation[0];
-            this->translation[1] = translation[1];
-            this->translation[2] = translation[2];
-            
-            this->rotation[0] = rotation[0];
-            this->rotation[1] = rotation[1];
-            this->rotation[2] = rotation[2];
-            
-            this->scale[0] = scale[0];
-            this->scale[1] = scale[1];
-            this->scale[2] = scale[2];
-        }
+                const float rotation[3]);
+
+        void SetTranslation(const float x, const float y, const float z);
+        void TranslateBy(const float x, const float y, const float z);
+        void SetScale(const float x, const float y, const float z);
+        void ScaleBy(const float x, const float y, const float z);
+        void SetRotation(const float x, const float y, const float z);
+        void RotateBy(const float x, const float y, const float z);
 
         XMMATRIX GetModelMatrix() const;
         Transform operator+(const Transform& other);
-        Transform& operator=(const Transform& other)
-        {
-            this->translation[0] = other.translation[0];
-            this->translation[1] = other.translation[1];
-            this->translation[2] = other.translation[2];
-            
-            this->rotation[0] = other.rotation[0];
-            this->rotation[1] = other.rotation[1];
-            this->rotation[2] = other.rotation[2];
-            
-            this->scale[0] = other.scale[0];
-            this->scale[1] = other.scale[1];
-            this->scale[2] = other.scale[2];
-
-            return *this;
-        };
+        Transform& operator=(const Transform& other);
     };
 
 }
